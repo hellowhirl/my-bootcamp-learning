428e3ff93f614426d682761c3ec44b01e4a383a4
Anthony Avila <hellowhirl@users.noreply.github.com>
2019-10-30 18:56:05 +0900

    add JS notes for Arrays - removing elements, emptying an array, combining and slicing arrays, spread operator, different ways to iterate over an array, joining arrays, sorting arrays, testing with .every() and .some(), filtering an array

--- js-basics/arrays.js
+++ js-basics/arrays.js
@@ -13,6 +13,7 @@ numbers.splice(3, 0, 'a', 'b')
 console.log(numbers);
 
 
+
 // Finding elements in an array
 // Primitives
 
@@ -30,6 +31,7 @@ console.log(example.includes(1)); // better, new method
 console.log(example.indexOf(1, 3)) // the second argument changes position for start of search
 
 
+
 // Finding elements in an array
 // Objects
 
@@ -43,6 +45,8 @@ const course = courses.find(function(course){ // this is a callback function (or
     // return course.name === 'xyz'; // will return undefined
 })
 
+// callback is used here because it is called back as part of finding an elememnt in the array
+
 const findCourseIndex = courses.findIndex(function(course){ // will return index
     return course.name === 'a'; // 0
 })
@@ -50,7 +54,6 @@ const findCourseIndex = courses.findIndex(function(course){ // will return index
 console.log(course);
 console.log(findCourseIndex);
 
-// callback is used here because it is called back as part of finding an elememnt in the array
 
 
 // Arrow functions:
@@ -65,3 +68,186 @@ const arrowCourse = courses.find(course => course.name === 'a' )
 
 // with no parameters
 const otherCourse = courses.find(() => { course.name === 'a' })
+
+
+
+// Removing elements
+
+const moreNumbers = [1, 2, 3, 4];
+
+// Remove from end
+const last = moreNumbers.pop();
+console.log(moreNumbers); // 1, 2, 3
+console.log(last); // 4
+
+// Remove from begining
+const start = moreNumbers.shift();
+console.log(moreNumbers); // 2, 3, 4
+console.log(start); // 1
+
+// Remove from middle
+const extraNumbers = [1, 2, 3, 4, 5];
+const middle = extraNumbers.splice(2, 2); // again we use splice method, but without adding fial arguments
+console.log(extraNumbers); // 1, 2, 5
+console.log(middle); // 3, 4 - from index 2 removed 2 elements
+
+
+
+// Emptying an array - 4 solutions
+
+let emptyNumbers = [1, 2, 3, 4];
+let another = emptyNumbers; // will still point to old object of 'emptyNumbers'
+console.log(emptyNumbers);
+
+// Solution 1
+emptyNumbers = []; // good, but there is catch where there could still be reference to original array
+console.log('another:', another);
+
+// Solution 2
+emptyNumbers.length = 0; // most recommended (usually trumps Solution 1, but sometimes both are OK)
+
+// Solution 3
+emptyNumbers.splice(0, emptyNumbers.length);
+
+// Solution 4
+while (emptyNumbers.length > 0)
+    emptyNumbers.pop();
+
+console.log(emptyNumbers);
+
+
+
+// Combining and slicing arrays
+
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second); // combine arrays
+console.log(combined);
+
+
+let slice = combined.slice(2, 4); // 2 arguments indicate start index and end index that we will extract out of array
+console.log(slice); // [3, 4]
+
+slice = combined.slice(2); // 1 argument excluding end idnex - we get all the elements in original array starting from this index
+console.log(slice);
+
+slice = combined.slice(); // no arguments - we get copy of original array
+console.log('slice() get copy of original array', slice);
+
+
+// if values are Primitive they are copied by Value
+// with combining objects (References) they are copied by their reference
+
+const third = [{ id: 1}]
+const combineAgain = third.concat(second); // 'third' object is not copied, only its reference is copied to the combined array
+
+third[0].id = 20; // go to the first array, get the first elemeent (which is the object) and change its id to 20
+console.log(combineAgain);
+
+
+
+// Spread operator
+
+const betterCombination = [...first, ...second];
+console.log('betterCombination', betterCombination);
+
+const copy = [...betterCombination];
+console.log(copy);
+
+
+
+// Iterating an array (some different ways)
+
+for(let number of numbers)
+    console.log('for of', number)
+
+numbers.forEach(function(number) {
+    console.log('forEach', number);
+})
+
+numbers.forEach(number => console.log(number)); // forEach as Arrow function style
+
+numbers.forEach((number, index) => console.log(number, index)); // we can add second parameter for index
+
+
+
+// Joining arrays
+
+// tooltips (parameter?) with a question mark means it's optional
+
+const someNumbers = [1, 2, 3];
+const joined = someNumbers.join(', ' );
+console.log(joined);
+
+const someMessage = "This is my first message";
+const splitMessage = someMessage.split(' ');
+console.log(splitMessage);
+
+const hyphenMessage = splitMessage.join('-');
+console.log(hyphenMessage);
+
+
+
+// Sorting arrays
+
+const mixedNumbers = [2, 3, 1];
+mixedNumbers.sort(); // [1, 2, 3]
+console.log(mixedNumbers)
+
+mixedNumbers.reverse();
+console.log(mixedNumbers) // [3, 2, 1]
+
+
+
+// ASCII: American Standard Code for Information Interchange
+
+// comparing strings
+'a' < 'b' // true
+'a' > 'b' // false
+
+const classes = [
+    {id: 1, name: 'Node.js'},
+    {id: 2, name: 'javaScript'}
+]
+
+classes.sort(function(a, b) {
+    const classA = a.name.toUpperCase(); // to overcome differences in capitalized and lowercase
+    const classB = b.name.toUpperCase();
+
+    if(classA > classB) return 1; // this will compare the starting character's decimal value accoording to ascii table
+    if(classB > classA) return -1;
+    return 0;
+})
+
+console.log(classes);
+
+
+
+// Testing the elements of an array (new methods in JS)
+
+const testArray = [1, 2, 3, -1];
+
+const allPositive = testArray.every(function(value) { // every() - checks if every element in array matches criteria
+    return value >= 0;
+})
+console.log(allPositive);
+
+const atLeastOnePositive = testArray.some(function(value) { // some() - if at least one value matches criteria
+    return value >= 0;    
+})
+console.log(atLeastOnePositive);
+
+
+
+// Filtering an array (based on a search criteria)
+
+const againNumbers = [1, -1, 2, 3];
+
+// const filterArray = againNumbers.filter(function(value) {
+//     return value >= 0;
+// })
+
+const filteredArray = againNumbers.filter(n => n >= 0) // made into arrow function, we can exclude 'return'
+
+console.log('filteredArray', filteredArray); // returns a new array of values according to filter criteria
