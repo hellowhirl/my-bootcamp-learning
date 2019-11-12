55f5ab079e1b4d579eb85b68004789424d9a6aa2
Anthony Avila <hellowhirl@users.noreply.github.com>
2019-11-12 04:20:44 -0800

    React notes, start new project component basics, embedding expressions, settig attributes dynamically

---
+++ counter-app/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
+
+// instead of App component we can render our Counter component - determined by what is returned from render method in component
+ReactDOM.render(<Counter />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
