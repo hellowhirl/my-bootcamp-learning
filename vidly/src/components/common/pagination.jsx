df44925bdf91864880f06fb6a6ef38b5bb10a670
Anthony Avila <hellowhirl@users.noreply.github.com>
2019-12-09 17:20:18 +0900

    React - pagination using lodash

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import _ from "lodash"; // "_" is common convention because lodash is optimized version of library called "underscore"
 
 const Pagination = props => {
   console.log(props.itemsCount);
-  let totalPages = Math.ceil(props.itemsCount / props.pageSize);
-  console.log(totalPages);
+  const totalPages = Math.ceil(props.itemsCount / props.pageSize);
+  if (totalPages === 1) return null; // edge case
+  const pages = _.range(1, totalPages + 1); // add 1 to make sure last page is also included
 
   return (
-    <nav aria-label="Page navigation example">
+    <nav>
       <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link" href="#">
-            1
-          </a>
-        </li>
+        {pages.map(page => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
