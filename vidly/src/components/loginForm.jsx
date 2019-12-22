511dc988e521faa4fc9aa44ae802cfd56f457fe4
Anthony Avila <hellowhirl@users.noreply.github.com>
2019-12-22 20:12:41 +0900

    React - validating a form using Joi library

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -11,16 +12,25 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Usernmae"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
   validate = () => {
+    const options = { abortEarly: false }; // our 3rd argument - code becomes more expressive
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
     const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    // one approach to map an array into an object
+    // also able to use .map() or .reduce()
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   // better approach is to use 'autoFocus'
@@ -44,11 +54,29 @@ class LoginForm extends Component {
     console.log("submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+      // ...other rules...
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+      // ...other rules...
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    // live validation after onChange event is triggered
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    // if no error message then we should delete the existing property so the error is cleared up
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     // give each field a 'name' property for bracket notation
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
