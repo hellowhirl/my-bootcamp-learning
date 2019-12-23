895404f28d177d356db2a21453201f3399a30278
Anthony Avila <hellowhirl@users.noreply.github.com>
2019-12-23 12:58:26 +0900

    React - refactor form into reusable component, extends to our own custom base class

---
+++ vidly/src/components/common/form.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {}
+  };
+
+  validate = () => {
+    const options = { abortEarly: false }; // our 3rd argument - code becomes more expressive
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    // one approach to map an array into an object
+    // also able to use .map() or .reduce()
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value }; // what ever 'name' is used at runtime, that will be used to set the key
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema); // no need for 3rd argument, we don't want to abort early
+
+    // ternary operator below 2 lines:
+    // if (error) return null;
+    // return error.details[0].message;
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    // Call the server --> save the changes --> redirect user to different page
+    // const username = document.getElementById('username').nodeValue; // this is the plain JS approach
+
+    // if you really need to access the DOM, this is the way to do it
+    // should not over use this method
+    // const username = this.username.current.value;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    // live validation after onChange event is triggered
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    // if no error message then we should delete the existing property so the error is cleared up
+    else delete errors[input.name];
+
+    const data = { ...this.state.data };
+    // give each field a 'name' property for bracket notation
+    data[input.name] = input.value;
+    this.setState({ data, errors });
+  };
+}
+
+export default Form;
--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./common/input";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   // accessing a DOM element in React: defining a property for 'ref' by creating a ref object
   //   username = React.createRef();
 
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     // the properties in 'error' object map to the name of our input fields
     errors: {}
   };
@@ -21,66 +22,18 @@ class LoginForm extends Component {
       .label("Password")
   };
 
-  validate = () => {
-    const options = { abortEarly: false }; // our 3rd argument - code becomes more expressive
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null;
-
-    const errors = {};
-    // one approach to map an array into an object
-    // also able to use .map() or .reduce()
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
   // better approach is to use 'autoFocus'
   //   componentDidMount() {
   //     this.username.current.focus();
   //   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
-    // Call the server --> save the changes --> redirect user to different page
-    // const username = document.getElementById('username').nodeValue; // this is the plain JS approach
-
-    // if you really need to access the DOM, this is the way to do it
-    // should not over use this method
-    // const username = this.username.current.value;
+  doSubmit = () => {
+    // call the server
     console.log("submitted");
   };
 
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value }; // what ever 'name' is used at runtime, that will be used to set the key
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema); // no need for 3rd argument, we don't want to abort early
-
-    // ternary operator below 2 lines:
-    // if (error) return null;
-    // return error.details[0].message;
-    return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    // live validation after onChange event is triggered
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    // if no error message then we should delete the existing property so the error is cleared up
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    // give each field a 'name' property for bracket notation
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -88,14 +41,14 @@ class LoginForm extends Component {
           <Input
             name="username"
             label="Username"
-            value={account.username}
+            value={data.username}
             onChange={this.handleChange}
             error={errors.username}
           />
           <Input
             name="password"
             label="Password"
-            value={account.password}
+            value={data.password}
             onChange={this.handleChange}
             error={errors.password}
           />
