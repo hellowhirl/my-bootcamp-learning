afa7d6ab8d6295bbe145550545fa897dde6fa668
Anthony Avila <hellowhirl@users.noreply.github.com>
2019-12-23 19:33:55 +0900

    React - extract a method for rendering an input, return markup from Input, render type default and dynamically, use rest operator and spread operato to reduce repitition with adding and setting parametersr

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange, error }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value} // this input field will no longer have its own state - using props to set its value
-        autoFocus // this solution is better for giving input field focus
-        onChange={onChange}
-        // ref={this.username}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} id={name} name={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
