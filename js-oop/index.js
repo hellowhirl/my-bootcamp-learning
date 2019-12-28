d8a6e9bd6e74a10bfedcfa45981b113c12d916a7
Anthony Avila <hellowhirl@users.noreply.github.com>
2019-12-28 23:07:53 +0900

    JS Object-Oriented Programming - notes

---
+++ js-oop/index.js
@@ -0,0 +1,10 @@
+console.log("hi oop");
+
+// function addDiv() {
+//   const divy = document.createElement("h1");
+//   divy.innerHTML = "hello oop";
+
+//   document.body.appendChild(divy);
+// }
+
+// addDiv();
