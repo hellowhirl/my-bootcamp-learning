199eb190e0b45ce7e3c003aaf2f615ccfbde4683
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-01-02 18:41:13 +0900

    JS OOP exercise - PREMATURE OPTIMIZATION IS THE ROOT OF ALL EVIL

--- js-basics/arrays.js
+++ js-basics/arrays.js
@@ -1,4 +1,3 @@
-
 const numbers = [3, 4]; // unable to assign the values, but we can change use Array object methods
 
 // Add to end
@@ -8,53 +7,49 @@ numbers.push(5, 6);
 numbers.unshift(1, 2);
 
 // Add to middle
-numbers.splice(3, 0, 'a', 'b')
+numbers.splice(3, 0, "a", "b");
 
 console.log(numbers);
-
-
 
 // Finding elements in an array
 // Primitives
 
-const example = [1, 2, 3, 1, 4]
+const example = [1, 2, 3, 1, 4];
 
-console.log(example.indexOf('a'));
-console.log(example.indexOf(1));
-console.log(example.indexOf('1'));
+console.log(example.indexOf(6)); // -1
+console.log(example.indexOf(3)); // 0
+console.log(example.indexOf("1")); // -1
 
 console.log(example.lastIndexOf(1));
 
 console.log(example.indexOf(1) !== -1); // works but it's ugly
 console.log(example.includes(1)); // better, new method
 
-console.log(example.indexOf(1, 3)) // the second argument changes position for start of search
-
-
+console.log(example.indexOf(1, 3)); // the second argument changes position for start of search
 
 // Finding elements in an array
 // Objects
 
 const courses = [
-    {id: 1, name: 'a'},
-    {id: 2, name: 'b'}
+  { id: 1, name: "a" },
+  { id: 2, name: "b" }
 ];
 
-const course = courses.find(function(course){ // this is a callback function (or predicate) 
-    return course.name === 'a'; // only gets first element that matches this criteria
-    // return course.name === 'xyz'; // will return undefined
-})
+const course = courses.find(function(course) {
+  // this is a callback function (or predicate)
+  return course.name === "a"; // only gets first element that matches this criteria
+  // return course.name === 'xyz'; // will return undefined
+});
 
 // callback is used here because it is called back as part of finding an elememnt in the array
 
-const findCourseIndex = courses.findIndex(function(course){ // will return index
-    return course.name === 'a'; // 0
-})
+const findCourseIndex = courses.findIndex(function(course) {
+  // will return index
+  return course.name === "a"; // 0
+});
 
 console.log(course);
 console.log(findCourseIndex);
-
-
 
 // Arrow functions:
 // whenever we want to pass a function as a callback function or as an argument for a different method
@@ -62,14 +57,14 @@ console.log(findCourseIndex);
 // remove function keyword, separate parameters of function from body with =>
 // with single parameter we can get rid of the paranthesis
 // if single line of code (single value, etc.) can be written in one line, remove curly braces and 'return' statement
-const arrowCourse = courses.find(course => course.name === 'a' )
-
+const arrowCourse = courses.find(course => course.name === "a");
+console.log("arrowCourse", arrowCourse);
 // we read this expression as "course goes to course.name === a" - we are finding a course with name equal to 'a'
 
 // with no parameters
-const otherCourse = courses.find(() => { course.name === 'a' })
-
-
+const otherCourse = courses.find(() => {
+  course.name === "a";
+});
 
 // Removing elements
 
@@ -91,8 +86,6 @@ const middle = extraNumbers.splice(2, 2); // again we use splice method, but wit
 console.log(extraNumbers); // 1, 2, 5
 console.log(middle); // 3, 4 - from index 2 removed 2 elements
 
-
-
 // Emptying an array - 4 solutions
 
 let emptyNumbers = [1, 2, 3, 4];
@@ -101,7 +94,7 @@ console.log(emptyNumbers);
 
 // Solution 1
 emptyNumbers = []; // good, but there is catch where there could still be reference to original array
-console.log('another:', another);
+console.log("another:", another);
 
 // Solution 2
 emptyNumbers.length = 0; // most recommended (usually trumps Solution 1, but sometimes both are OK)
@@ -110,12 +103,9 @@ emptyNumbers.length = 0; // most recommended (usually trumps Solution 1, but som
 emptyNumbers.splice(0, emptyNumbers.length);
 
 // Solution 4
-while (emptyNumbers.length > 0)
-    emptyNumbers.pop();
+while (emptyNumbers.length > 0) emptyNumbers.pop();
 
 console.log(emptyNumbers);
-
-
 
 // Combining and slicing arrays
 
@@ -125,7 +115,6 @@ const second = [4, 5, 6];
 const combined = first.concat(second); // combine arrays
 console.log(combined);
 
-
 let slice = combined.slice(2, 4); // 2 arguments indicate start index and end index that we will extract out of array
 console.log(slice); // [3, 4]
 
@@ -133,112 +122,98 @@ slice = combined.slice(2); // 1 argument excluding end idnex - we get all the el
 console.log(slice);
 
 slice = combined.slice(); // no arguments - we get copy of original array
-console.log('slice() get copy of original array', slice);
-
+console.log("slice() get copy of original array", slice);
 
 // if values are Primitive they are copied by Value
 // with combining objects (References) they are copied by their reference
 
-const third = [{ id: 1}]
+const third = [{ id: 1 }];
 const combineAgain = third.concat(second); // 'third' object is not copied, only its reference is copied to the combined array
 
 third[0].id = 20; // go to the first array, get the first elemeent (which is the object) and change its id to 20
 console.log(combineAgain);
 
-
-
 // Spread operator
 
 const betterCombination = [...first, ...second];
-console.log('betterCombination', betterCombination);
+console.log("betterCombination", betterCombination);
 
 const copy = [...betterCombination]; // clones (or copy) of original array
 console.log(copy);
 
-
-
 // Iterating an array (some different ways)
 
-for(let number of numbers)
-    console.log('for of', number)
+for (let number of numbers) console.log("for of", number);
 
 numbers.forEach(function(number) {
-    console.log('forEach', number);
-})
+  console.log("forEach", number);
+});
 
 numbers.forEach(number => console.log(number)); // forEach as Arrow function style
 
 numbers.forEach((number, index) => console.log(number, index)); // we can add second parameter for index
-
-
 
 // Joining arrays
 
 // tooltips (parameter?) with a question mark means it's optional
 
 const someNumbers = [1, 2, 3];
-const joined = someNumbers.join(', ' );
+const joined = someNumbers.join(", ");
 console.log(joined);
 
 const someMessage = "This is my first message";
-const splitMessage = someMessage.split(' ');
+const splitMessage = someMessage.split(" ");
 console.log(splitMessage);
 
-const hyphenMessage = splitMessage.join('-');
+const hyphenMessage = splitMessage.join("-");
 console.log(hyphenMessage);
-
-
 
 // Sorting arrays
 
 const mixedNumbers = [2, 3, 1];
 mixedNumbers.sort(); // [1, 2, 3]
-console.log(mixedNumbers)
+console.log(mixedNumbers);
 
 mixedNumbers.reverse();
-console.log(mixedNumbers) // [3, 2, 1]
-
-
+console.log(mixedNumbers); // [3, 2, 1]
 
 // ASCII: American Standard Code for Information Interchange
 
 // comparing strings
-'a' < 'b' // true
-'a' > 'b' // false
+"a" < "b"; // true
+"a" > "b"; // false
 
 const classes = [
-    {id: 1, name: 'Node.js'},
-    {id: 2, name: 'javaScript'}
-]
+  { id: 1, name: "Node.js" },
+  { id: 2, name: "javaScript" }
+];
 
 classes.sort(function(a, b) {
-    const classA = a.name.toUpperCase(); // to overcome differences in capitalized and lowercase
-    const classB = b.name.toUpperCase();
+  const classA = a.name.toUpperCase(); // to overcome differences in capitalized and lowercase
+  const classB = b.name.toUpperCase();
 
-    if(classA > classB) return 1; // this will compare the starting character's decimal value accoording to ascii table
-    if(classB > classA) return -1;
-    return 0;
-})
+  if (classA > classB) return 1; // this will compare the starting character's decimal value accoording to ascii table
+  if (classB > classA) return -1;
+  return 0;
+});
 
 console.log(classes);
-
-
 
 // Testing the elements of an array (new methods in JS)
 
 const testArray = [1, 2, 3, -1];
 
-const allPositive = testArray.every(function(value) { // every() - checks if every element in array matches criteria
-    return value >= 0;
-})
+const allPositive = testArray.every(function(value) {
+  // every() - checks if every element in array matches criteria
+  return value >= 0;
+});
 console.log(allPositive);
 
-const atLeastOnePositive = testArray.some(function(value) { // some() - if at least one value matches criteria
-    return value >= 0;    
-})
+const atLeastOnePositive = testArray.some(function(value) {
+  // some() - if at least one value matches criteria
+  return value >= 0;
+});
 console.log(atLeastOnePositive);
-
-
 
 // Filtering an array (based on a search criteria)
 
@@ -248,11 +223,9 @@ const againNumbers = [1, -1, 2, 3];
 //     return value >= 0;
 // })
 
-const filteredArray = againNumbers.filter(n => n >= 0) // made into arrow function, we can exclude 'return'
+const filteredArray = againNumbers.filter(n => n >= 0); // made into arrow function, we can exclude 'return'
 
-console.log('filteredArray', filteredArray); // returns a new array of values according to filter criteria
-
-
+console.log("filteredArray", filteredArray); // returns a new array of values according to filter criteria
 
 console.log(`************************
 *** Mapping an array ***
@@ -266,38 +239,36 @@ const newNumbers = [1, 2, 3, -1];
 
 const newFiltered = newNumbers.filter(n => n >= 0);
 
-const items = newFiltered.map(n => '<li>' + n + '</li>' );
-const html = '<ul>' + items.join('') + '</ul>';
+const items = newFiltered.map(n => "<li>" + n + "</li>");
+const html = "<ul>" + items.join("") + "</ul>";
 console.log(items);
 console.log(html);
 
 // object example
 
 const objectItems = newFiltered.map(n => {
-    const obj = {value: n};
-    return obj;
+  const obj = { value: n };
+  return obj;
 });
 
-console.log('objectItems', objectItems);
+console.log("objectItems", objectItems);
 
 // chainable methods - when result from a constant is not used anywhere throughout your code (no need to declare 'const obj')
 // in this case we can simply return object without declaring a constant
 // should put each method call on a seperate line for cleaner code
 
-const objectItemImproved = newFiltered.map(n => ({value: n}));
+const objectItemImproved = newFiltered.map(n => ({ value: n }));
 
-console.log('objectItemImproved', objectItemImproved);
+console.log("objectItemImproved", objectItemImproved);
 
 const chainedMethods = newFiltered
-    .filter(n => n >= 0)
-    .map(n => ({value: n}))
-    .filter(obj => obj.value > 1)
-    .map(obj => obj.value);
-    // .filter(n => n >= 0)
+  .filter(n => n >= 0)
+  .map(n => ({ value: n }))
+  .filter(obj => obj.value > 1)
+  .map(obj => obj.value);
+// .filter(n => n >= 0)
 // .map(n => ({ value: n }))
 console.log(chainedMethods);
-
-
 
 // Reducing an array (to single value)
 
@@ -305,14 +276,12 @@ const purchaseNumbers = [1, -1, 2, 3];
 
 // old way:
 let sum = 0;
-for(let n of purchaseNumbers)
-    sum += n;
+for (let n of purchaseNumbers) sum += n;
 console.log(sum);
 
 // let sumOfReduced = purchaseNumbers.reduce((accumulatr, currentValue) => {
 //     return accumulatr + currentValue;
 // }, 100); // '0' is initial value for accumulator
-
 
 // more elegant way:
 
@@ -322,12 +291,13 @@ console.log(sum);
 // a = 2, c = 2, a = 2
 // a = 2, c = 3, a = 5
 let sumOfReduced = purchaseNumbers.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
+  return accumulator + currentValue;
 }, 100); // '100' is initial value for accumulator - default is '0' if left unset
 
 console.log(sumOfReduced);
 
-
-let betterSumOfReduced = purchaseNumbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+let betterSumOfReduced = purchaseNumbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
 
 console.log(betterSumOfReduced);
--- js-oop/stopwatch.js
+++ js-oop/stopwatch.js
@@ -1,38 +1,80 @@
 // constructor function
 function Stopwatch() {
-  let startTime = 0;
-  let stopTime = 0;
-  let totalTime = 0;
-  let startClock = false;
-  this.start = function() {
-    if (startClock) throw new Error("Stopwatch has already started!");
-    startClock = true;
-    startTime = new Date().getTime();
-    startTime = (startTime % (1000 * 60)) / 1000;
-  };
-
-  this.stop = function() {
-    if (!startClock) throw new Error("Stopwatch is not started!");
-    startClock = false;
-
-    stopTime = new Date().getTime();
-    stopTime = (stopTime % (1000 * 60)) / 1000;
-
-    totalTime += stopTime - startTime;
-
-    startTime = 0;
-    stopTime = 0;
-  };
-
+  // let startTime = 0;
+  // let stopTime = 0;
+  // let totalTime = 0;
+  // let startClock = false;
+  // this.start = function() {
+  //   if (startClock) throw new Error("Stopwatch has already started!");
+  //   startClock = true;
+  //   startTime = new Date().getTime();
+  //   startTime = (startTime % (1000 * 60)) / 1000;
+  // };
+  // this.stop = function() {
+  //   if (!startClock) throw new Error("Stopwatch is not started!");
+  //   startClock = false;
+  //   stopTime = new Date().getTime();
+  //   stopTime = (stopTime % (1000 * 60)) / 1000;
+  //   totalTime += stopTime - startTime;
+  //   startTime = 0;
+  //   stopTime = 0;
+  // };
   Object.defineProperty(this, "duration", {
     get: function() {
-      return totalTime;
+      return this.totalTime;
     }
   });
-
-  this.reset = function() {
-    totalTime = 0;
-  };
+  Object.defineProperty(this, "startTime", {
+    get: function() {
+      return startTime;
+    }
+  });
+  // Object.defineProperty(this, "stopTime", {
+  //   get: function() {
+  //     return stopTime;
+  //   }
+  // });
+  // Object.defineProperty(this, "startClock", {
+  //   get: function() {
+  //     return startClock;
+  //   }
+  // });
+  // this.reset = function() {
+  //   totalTime = 0;
+  // };
 }
+
+// The point of this exercise:
+// Putting all the methods on the prototype was a bad idea to start with, so
+// PREMATURE OPTIMIZATION IS THE ROOT OF ALL EVIL
+
+Stopwatch.prototype.startTime = 0;
+Stopwatch.prototype.stopTime = 0;
+Stopwatch.prototype.totalTime = 0;
+Stopwatch.prototype.startClock = false;
+
+Stopwatch.prototype.start = function() {
+  if (this.startClock) throw new Error("Stopwatch has already started!");
+  this.startClock = true;
+  this.startTime = new Date().getTime();
+  this.startTime = (this.startTime % (1000 * 60)) / 1000;
+};
+
+Stopwatch.prototype.stop = function() {
+  if (!this.startClock) throw new Error("Stopwatch is not started!");
+  this.startClock = false;
+
+  this.stopTime = new Date().getTime();
+  this.stopTime = (this.stopTime % (1000 * 60)) / 1000;
+
+  this.totalTime += this.stopTime - this.startTime;
+
+  this.startTime = 0;
+  this.stopTime = 0;
+};
+
+Stopwatch.prototype.reset = function() {
+  this.totalTime = 0;
+};
 
 const sw = new Stopwatch();
