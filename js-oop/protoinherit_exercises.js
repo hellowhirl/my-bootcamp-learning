8d9be4a6757f2a7fa7968c3fe669deff849a22dc
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-01-05 20:40:16 +0900

    JS OOP exercise - Prototypical Inheritance, solution & notes

--- js-oop/protoinherit_exercises.js
+++ js-oop/protoinherit_exercises.js
@@ -19,26 +19,14 @@ HtmlElement.prototype.focus = function() {
 //   }
 // };
 
-function HtmlSelectELement(...args) {
-  let items = [] && [...args].flat();
-  this.addItem = function(number) {
-    items.push(number);
-    //
+function HtmlSelectELement(items = []) {
+  this.items = items;
+  this.addItem = function(item) {
+    items.push(item);
   };
-  this.removeItem = function(number) {
-    items = items.filter(n => n !== number);
+  this.removeItem = function(item) {
+    this.items.splice(this.items.indexOf(item), 1);
   };
-
-  //   HtmlElement.call(this, this.click);
-
-  //   HtmlElement.call(this, click);
-
-  Object.defineProperty(this, "items", {
-    get: function(value) {
-      //   console.log(arr);
-      if (typeof value !== undefined) return items;
-    }
-  });
 }
 
 HtmlSelectELement.prototype = {
@@ -48,10 +36,17 @@ HtmlSelectELement.prototype = {
   }
 };
 
-// Object.assign(HtmlElement.prototype, clickable);
+// below creates an empty object that has its prototype set to HtmlElement - with this impolementation
+// we do not inherit the own method of click on the HtmlElement object to HtmlSelectElement
+// HtmlSelectElement.prototype = Object.assign(HtmlElement.prototype, clickable);
+
+// here we set the prototype of an object to an instance of an object
 HtmlSelectELement.prototype = new HtmlElement();
+HtmlSelectELement.prototype.constructor = HtmlSelectELement;
+
+// implementing inheritance is far easier in ES6
 
 const e = new HtmlElement();
-const h = new HtmlSelectELement([1, 2, 3]);
+const h = new HtmlSelectELement([1, 2, 3, 4, 5]);
 
 console.log(h);
