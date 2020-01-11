8e4b878b5df01bd7390ac9c9de8f78d6e60efce0
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-01-11 22:19:02 +0900

    clean up format in notes for controlFlow and objects

--- js-basics/controlFlow.js
+++ js-basics/controlFlow.js
@@ -4,61 +4,46 @@
 
 let hour = 13;
 
-if (hour >= 6 && hour < 12 )
-    console.log('Good morning'); 
-else if (hour >= 12 && hour < 18)
-    console.log('Good noon')
-else
-    console.log('Good evening');
-
-
+if (hour >= 6 && hour < 12) console.log("Good morning");
+else if (hour >= 12 && hour < 18) console.log("Good noon");
+else console.log("Good evening");
 
 let role;
-role = 'guest';
+role = "guest";
 
 // Switch & Case: a little bit outdated - but depends on preference
 
 switch (role) {
-    case 'guest':
-        console.log('guest user');
-        break; // to jump out of switch block
+  case "guest":
+    console.log("guest user");
+    break; // to jump out of switch block
 
-    case 'moderator':
-        console.log('moderator user');
-        break;
+  case "moderator":
+    console.log("moderator user");
+    break;
 
-    default:
-        console.log('unknown role');
+  default:
+    console.log("unknown role");
 }
 
-
-
-if (role === 'guest') console.log('guest user');
-else if (role === 'moderator') console.log('moderator user');
-else console.log('unknown user');
-
-
-
+if (role === "guest") console.log("guest user");
+else if (role === "moderator") console.log("moderator user");
+else console.log("unknown user");
 
 // for loops: require 3 statements
 // - initial expression (i), declare and initialize a variable. This is known as the 'loop variable'
 // - a condition, will execute as long as it evaluates to true
 // - increment expression, use the increment operator
 
-for (let i = 0; i < 5; i++)
-    console.log('hello 5 times', i);
+for (let i = 0; i < 5; i++) console.log("hello 5 times", i);
 
 // OR ALTERNATIVELY
 
-for (let i = 1; i <=5; i++)
-    console.log('hello alternative', i);
+for (let i = 1; i <= 5; i++) console.log("hello alternative", i);
 
-for (let i = 1; i <=5; i++)
-    if (i % 2) console.log('odd numbers:', i);
+for (let i = 1; i <= 5; i++) if (i % 2) console.log("odd numbers:", i);
 
 // in certain problems though we may want to use a for loop in the reverse order
-
-
 
 // direct translation of above into whie loop
 
@@ -69,64 +54,54 @@ for (let i = 1; i <=5; i++)
 //     i++;
 // }
 
-
 // rewrite in do-while loop
 // do-while loops are always executed at least once
 // not so practical to use
 
 let i = 9;
 do {
-    if (i % 2) console.log('odd numbers bros:', i); // will only get carried out once
-    i++;
-} while (i <=5);
-
-
+  if (i % 2) console.log("odd numbers bros:", i); // will only get carried out once
+  i++;
+} while (i <= 5);
 
 // avoid infinite loops, don't forget to include increment expressions, etc.
-
 
 // 2 other loops in JavaScript: iterate over the properties of an object or elements in an array
 
 // for-in loop - ideal iterating over properties of an object
 
 const human = {
-    name: 'Mosh',
-    age: 30
+  name: "Mosh",
+  age: 30
 };
 
 for (let key in human) // in every iteration this 'key' variable in the loop will hold the name of one of the properties in object
-    console.log(`${key} is ${human[key]}`);
+  console.log(`${key} is ${human[key]}`);
 
-console.log('brackets:', human['name'])
-
+console.log("brackets:", human["name"]);
 
 // for-in loop for array - not ideal
 
-const colors = ['red', 'green', 'blue']
+const colors = ["red", "green", "blue"];
 
-for (let index in colors)
-    console.log(index, colors[index])    
-
+for (let index in colors) console.log(index, colors[index]);
 
 // for-of loop - ideal for iterating over elements/items in an array (since ES6)
 
-for (let color of colors)
-    console.log(color);
-
+for (let color of colors) console.log(color);
 
 // break and continue - can change the way a loop behaves
 
 let k = 0;
 while (k <= 10) {
-    if(k === 6) break; // a way to jump out of the loop
-    if(k % 2 === 0) {
-        k++;
-        continue // not recommended, just a legacy way to write things to watch out for
-    }
-    console.log(k);
+  if (k === 6) break; // a way to jump out of the loop
+  if (k % 2 === 0) {
     k++;
+    continue; // not recommended, just a legacy way to write things to watch out for
+  }
+  console.log(k);
+  k++;
 }
-
 
 // Exercise 1
 // function takes two numbers and returns the maximum of the two
@@ -136,24 +111,20 @@ let setNumbers = maximum(7, 6);
 console.log(setNumbers);
 
 function maximum(a, b) {
-    // if(a > b) return a;
-    // return b;
-    return a > b ? a : b; // cleanest inplementation
+  // if(a > b) return a;
+  // return b;
+  return a > b ? a : b; // cleanest inplementation
 }
-
-
 
 console.log(`is landscape?: ${isLandScape(40, 30)}`);
 
 // Exercise 2
 // Landscape or portrait
 
-function isLandScape (width, height) {
-    // return width > height ? true : false;
-    return (width > height); // not amateurish, compared to above
+function isLandScape(width, height) {
+  // return width > height ? true : false;
+  return width > height; // not amateurish, compared to above
 }
-
-
 
 // Exercise 3
 // Fizzbuzz
@@ -167,22 +138,15 @@ function isLandScape (width, height) {
 const output = fizzbuzz(15);
 console.log(output);
 
-function fizzbuzz (input) {
-    if ( typeof(input) !== 'number')
-        return NaN;
+function fizzbuzz(input) {
+  if (typeof input !== "number") return NaN;
 
-    if (input === 0)
-        return input;
-    if ( (input % 3 === 0) && (input % 5 === 0) )
-        return `${input}: FizzBuzz`;
-    if ( input % 3 === 0 )
-        return `${input}: Fizz`;
-    if ( input % 5 === 0 )
-        return `${input}: Buzz`;
-    return input;
+  if (input === 0) return input;
+  if (input % 3 === 0 && input % 5 === 0) return `${input}: FizzBuzz`;
+  if (input % 3 === 0) return `${input}: Fizz`;
+  if (input % 5 === 0) return `${input}: Buzz`;
+  return input;
 }
-
-
 
 // Exercise 4
 // Speed limit = 70
@@ -192,23 +156,20 @@ function fizzbuzz (input) {
 speedChecker(85);
 
 function speedChecker(speed) {
-    const speedLimit = 70; // avoid using 'magic numbers' in our code
-    const kmPerPoint = 5;
+  const speedLimit = 70; // avoid using 'magic numbers' in our code
+  const kmPerPoint = 5;
 
-    if (speed < speedLimit + kmPerPoint) {
-        console.log('Ok');
-        return; // this will stop the rest of the function from executing, eliminating the need for else block
-    }
+  if (speed < speedLimit + kmPerPoint) {
+    console.log("Ok");
+    return; // this will stop the rest of the function from executing, eliminating the need for else block
+  }
 
-    const pointsToDriver = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (pointsToDriver >= 12)
-        console.log('license suspended')
-    else {
-        console.log(`points: ${pointsToDriver}`)
-    }
+  const pointsToDriver = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (pointsToDriver >= 12) console.log("license suspended");
+  else {
+    console.log(`points: ${pointsToDriver}`);
+  }
 }
-
-
 
 // Exercise 5
 // Calculate whether ODD or EVEN for a range of numbers
@@ -216,15 +177,14 @@ function speedChecker(speed) {
 showNumbers(10);
 
 function showNumbers(limit) {
-    for(let i = 0; i <= limit; i++) {
-        if (i%2 === 0) console.log(i, 'EVEN');
-        else console.log(i, 'ODD');
+  for (let i = 0; i <= limit; i++) {
+    if (i % 2 === 0) console.log(i, "EVEN");
+    else console.log(i, "ODD");
 
-        const message = (i % 2 === 0) ? 'EVEN' : 'ODD'; // a cleaner implementation and less noisy
-        console.log(i, message);
-    }
+    const message = i % 2 === 0 ? "EVEN" : "ODD"; // a cleaner implementation and less noisy
+    console.log(i, message);
+  }
 }
-
 
 // Exercise 6
 // Function that countrs truthy values
@@ -234,63 +194,59 @@ const array = [undefined, 0, 1, 2, 3, 9, 18];
 console.log(countTruthy(array));
 
 function countTruthy(array) {
-    let count = 0;
-    for (let value of array) 
-        if(value) count++;
-    return count;
+  let count = 0;
+  for (let value of array) if (value) count++;
+  return count;
 }
-
 
 // Exercise 7
 // String properties
 
 const book = {
-    contents: 'picture',
-    writer: undefined,
-    year: 2002,
-    title: 'poo'
+  contents: "picture",
+  writer: undefined,
+  year: 2002,
+  title: "poo"
 };
 
-showProperties(book)
+showProperties(book);
 
 function showProperties(obj) {
-    for(let key in obj) {
-        let checkType = typeof obj[key];
-        if(checkType === 'string') {
-            console.log(checkType, obj[key]);
-        }
+  for (let key in obj) {
+    let checkType = typeof obj[key];
+    if (checkType === "string") {
+      console.log(checkType, obj[key]);
     }
+  }
 }
 
-
-// Exercise 8 
+// Exercise 8
 // Sum of Multiples of 3 and 5
 
 console.log(sum(10));
 
 function sum(limit) {
-    let numbersList = [];
-    let sum = 0;
+  let numbersList = [];
+  let sum = 0;
 
-    for(let i = 1; i <= limit; i++) {
-        if(i%3 === 0 || i%5 === 0) {
-            sum += i;
-            numbersList.push(i);
-        }
+  for (let i = 1; i <= limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+      numbersList.push(i);
     }
-    console.log(numbersList);
+  }
+  console.log(numbersList);
 
-    // for(let j = 0; j <= numbersList.length; j++) {
-    //     sum += numbersList[j];
-    //     console.log(sum);
-    // }
+  // for(let j = 0; j <= numbersList.length; j++) {
+  //     sum += numbersList[j];
+  //     console.log(sum);
+  // }
 
-    // const addNumbers = (a, b) => a + b
-    // const sum = numbersList.reduce(addNumbers);
+  // const addNumbers = (a, b) => a + b
+  // const sum = numbersList.reduce(addNumbers);
 
-    return sum;
+  return sum;
 }
-
 
 // Exercise 9
 // Grades
@@ -302,22 +258,21 @@ console.log(calculateGrade(marks));
 // console.log(calculateAverage(marks));
 
 function calculateGrade(averageMarks) {
-    const average = calculateAverage(averageMarks);
-    if(average < 60) return 'F';
-    if(average < 70) return 'D';
-    if(average < 80) return 'C';
-    if(average < 90) return 'B';
-    return 'A'; 
+  const average = calculateAverage(averageMarks);
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
 }
 
 function calculateAverage(array) {
-    let sum = 0;
-    for(let value of array) {
-        sum += value;        
-    }
-    return sum / array.length;
+  let sum = 0;
+  for (let value of array) {
+    sum += value;
+  }
+  return sum / array.length;
 }
-
 
 // Exercise 10
 // Stars
@@ -325,26 +280,24 @@ function calculateAverage(array) {
 showStars(10);
 
 function showStars(rows) {
-    let starCount = '';
-    for(let i = 1; i <= rows; i++) {
-        starCount += '*'
-        console.log(starCount);
-    }
+  let starCount = "";
+  for (let i = 1; i <= rows; i++) {
+    starCount += "*";
+    console.log(starCount);
+  }
 }
-
 
 nestedStars(6);
 
 function nestedStars(rows) {
-    for(let row = 1; row <= rows; row++) {
-        let declareStars = '';
-        for(i = 0; i < row; i++) {
-            declareStars += '*'
-        }
-        console.log(declareStars);
+  for (let row = 1; row <= rows; row++) {
+    let declareStars = "";
+    for (i = 0; i < row; i++) {
+      declareStars += "*";
     }
+    console.log(declareStars);
+  }
 }
-
 
 // Exercise 11
 // Prime numbers
@@ -352,34 +305,31 @@ function nestedStars(rows) {
 // showPrimes(20);
 
 function showPrimes(limit) {
-    for(let number = 2; number <= limit; number++) {
-
-        let isPrime = true;
-        for(let factor = 2; factor < number; factor++) {
-            if(number % factor === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if(isPrime) console.log(number);
+  for (let number = 2; number <= limit; number++) {
+    let isPrime = true;
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isPrime = false;
+        break;
+      }
     }
-}
 
+    if (isPrime) console.log(number);
+  }
+}
 
 // Let's use the 'single responsibility principle'
 
 aShowPrimes(20);
 
 function aShowPrimes(limit) {
-    for(let number = 2; number <= limit; number++)
-        if(isPrime(number)) console.log(number);
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
 }
 
 function isPrime(number) {
-    for(let factor = 2; factor < number; factor++) 
-        if(number % factor === 0) 
-            return false;
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
 
-    return true;
+  return true;
 }
--- js-basics/objects.js
+++ js-basics/objects.js
@@ -1,37 +1,36 @@
-
 // with an object we can combine indepenent variables and functions
 
 const objectExample = {
-    radius: 1,
-    dimensions: {
-        width: 1,
-        height: 2
-    },
-    status: true,
-    draw: function(){
-        console.log('draw');
-    }
+  radius: 1,
+  dimensions: {
+    width: 1,
+    height: 2
+  },
+  status: true,
+  draw: function() {
+    console.log("draw");
+  }
 };
 
 objectExample.draw();
 
-
 // factory functions
-console.log('factory functions');
+console.log("factory functions");
 
 function createCircle() {
-    const circle = { // we dont' need this constant defined because we are not going to reference it anywhere
-        radius: 1,
-        dimensions: {
-            width: 1,
-            height: 2
-        },
-        status: true,
-        draw: function(){
-            console.log('draw');
-        }
-    };
-    return circle;
+  const circle = {
+    // we dont' need this constant defined because we are not going to reference it anywhere
+    radius: 1,
+    dimensions: {
+      width: 1,
+      height: 2
+    },
+    status: true,
+    draw: function() {
+      console.log("draw");
+    }
+  };
+  return circle;
 }
 
 // clearned up version of above factory function
@@ -39,62 +38,62 @@ function createCircle() {
 console.log(createBetterCircle(4));
 
 function createBetterCircle(radius) {
-    return { // better to write with return as every time we run this function we'll get a circle object
-        radius: radius,
-        draw: function(){
-            console.log('draw');
-        }
-    };
+  return {
+    // better to write with return as every time we run this function we'll get a circle object
+    radius: radius,
+    draw: function() {
+      console.log("draw");
+    }
+  };
 }
-
 
 // best example of factory function
 
 function createBestCircle(radius) {
-    return {
-        radius, // in modern JavaScript, if key and value are the same we can make code shorter by just putting the key (no value)
-        draw() { // shorter syntax can be similar to how we define a function outside of an object (just drop the function keyword)
-            console.log('draw');
-        }
-    };
+  return {
+    radius, // in modern JavaScript, if key and value are the same we can make code shorter by just putting the key (no value)
+    draw() {
+      // shorter syntax can be similar to how we define a function outside of an object (just drop the function keyword)
+      console.log("draw");
+    }
+  };
 }
 
 const circle1 = createBestCircle(1);
 
-console.log({circle1});
-
-
+console.log({ circle1 });
 
 // Constructor function: job is to construct/create an object
-// Pascal Notation: OneTwoThree 
+// Pascal Notation: OneTwoThree
 
-console.log('constructor functions');
+console.log("constructor functions");
 
 function Circle(radius) {
-    this.radius = radius; // in JavaScript 'this' is a reference to the object that is executing this code
-    this.draw = function() {
-        console.log('draw');
-    }
+  this.radius = radius; // in JavaScript 'this' is a reference to the object that is executing this code
+  this.draw = function() {
+    console.log("draw");
+  };
 }
 
-const newCircle = new Circle(1); 
+const newCircle = new Circle(1);
 
-// 'new' operator, 
-// 1) creates an empty JS object 
-// 2) will set this to point to this new empty object 
-// 3) it will return this new object from this function 
+// 'new' operator,
+// 1) creates an empty JS object
+// 2) will set this to point to this new empty object
+// 3) it will return this new object from this function
 
 // Note: just pick a pattern (might be better to go with factory functions)
-
 
 // But object cannot chnage a constant variable but they can be changed dynamically by adding or removing properties
 
 const circle2 = {
-    radius: 1
-}
+  radius: 1
+};
 
-circle2.example = 'text';
-circle2.draw = function() {console.log('test')};
+circle2.example = "text";
+circle2.draw = function() {
+  console.log("test");
+};
 
 // delete circle3.draw;
 delete circle2.example;
@@ -102,7 +101,6 @@ delete circle2.example;
 console.log(circle2);
 
 // circle2 = {}; // this will give an error
-
 
 // 'constructor' refers to the function that was used to construct/create that object
 
@@ -116,27 +114,28 @@ new String(); // '' "" ``
 new Boolean(); // true false
 new Number(); // 1, 2, 3, ...
 
-
 // below 2 functions create the same function object
 
 function Triangle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw");
+  };
 }
 
 // when we declare a function internally it's represented like this
-const Triangle1 = new Function('radius', `
+const Triangle1 = new Function(
+  "radius",
+  `
 this.radius = radius;
 this.draw = function() {
     console.log('draw');
 }
-`);
+`
+);
 
 const newTriangle = new Triangle(1);
 const newTriangle1 = new Triangle1(2);
-
 
 let a = 10;
 let b = a; // a's value is copied into new variable
@@ -144,63 +143,58 @@ let b = a; // a's value is copied into new variable
 a = 20;
 
 // these 2 variables are completely independent of each other
-console.log({a}); // 20
-console.log({b}); // 10
+console.log({ a }); // 20
+console.log({ b }); // 10
 
+console.log(
+  "when we use an object, that object is not stored in that variable - that object is stored somewhere in memory"
+);
 
-console.log('when we use an object, that object is not stored in that variable - that object is stored somewhere in memory');
-
-let x = {value: 30}; // the address of that memory location is stored inside this variable
+let x = { value: 30 }; // the address of that memory location is stored inside this variable
 let y = x; // the address (or reference) is copied
 
 console.log(x);
 console.log(y);
 
-
 let number = 10; // variable is independent of each other
 
 function increase(number) {
-    number++; // variable is independent of each other
+  number++; // variable is independent of each other
 }
 
 increase(number);
 console.log(number);
 
-
-let sales = {value: 20};
+let sales = { value: 20 };
 
 function bar(sales) {
-    sales.value++;
+  sales.value++;
 }
 
 bar(sales);
 console.log(sales);
 
 // Primitives are copied by their Value
-// Objects (Reference Types) are copied by their Reference Types 
+// Objects (Reference Types) are copied by their Reference Types
 
 // Reference Types: Objects, Functions, Array
 
-
 const trapezoid = {
-    radius: 1,
-    draw() {
-        console.log('draw');
-    }
+  color: "blue",
+  draw() {
+    console.log("draw");
+  }
 };
 
-for (let key in trapezoid) 
-    console.log(key, trapezoid[key]);
+for (let key in trapezoid) console.log(key, trapezoid[key]);
 
-for (let key of Object.keys(trapezoid))
-    console.log(key);
+for (let key of Object.keys(trapezoid)) console.log(key);
 
 for (let entry of Object.entries(trapezoid)) {
-    console.log(entry);
+  console.log(entry);
 }
 
-if ('radius' in trapezoid) console.log('yes');
-
+if ("color" in trapezoid) console.log("yes");
 
 // The simplest way to enumerate the properties in an object is using the 'for in' loop
 
@@ -209,22 +203,22 @@ if ('radius' in trapezoid) console.log('yes');
 
 // To see if a given property or method exists in an object we use the 'in' operator
 
-
-
 // Object.assign: takes all properties and methods in the source object and copies them into the new object {}
 const another = Object.assign({}, trapezoid);
 
-const another1 = Object.assign({
-    extra: 'words'
-}, trapezoid);
+const another1 = Object.assign(
+  {
+    extra: "words"
+  },
+  trapezoid
+);
 
 console.log(another);
 console.log(another1);
 
 // Most elegant way to clone an object
 const elegant = { ...trapezoid }; // it takes all the properties and method in the trapezoid object and puts them between {}
-console.log({elegant});
-
+console.log({ elegant });
 
 // JavaScript has a Garbage Collector - it finds variables & constants and deallocates the memory automatically
 
@@ -232,37 +226,35 @@ console.log({elegant});
 // Math object: when dealing with mathematical calculations just refer to Mozilla
 // Math.random(), Math.min(), Math.max(), Math.round(), Math.floor()
 
-const message = "Hi everyone out there"; 
-const split = message.split(' '); // With dot notation JS automatically wraps this String primitive with a String object 
-console.log(split); 
+const message = "Hi everyone out there";
+const split = message.split(" "); // With dot notation JS automatically wraps this String primitive with a String object
+console.log(split);
 
-const otherMessage = new String('Hello');
+const otherMessage = new String("Hello");
 
 console.log(message);
 console.log(otherMessage);
-console.log(message.includes('Hi'));
-
+console.log(message.includes("Hi"));
 
 // Template literals ``
 // concatenating is easier with placeholders ${}
 // we can add any expression that produces a value between these curly braces
 
-const tempLit = `${message}, this is message ${3 +5} for today.`;
+const tempLit = `${message}, this is message ${3 + 5} for today.`;
 console.log(tempLit);
-
 
 // Date objects
 
-const now = new Date(); 
+const now = new Date();
 
-const date1 = new Date('May 11 2018 09:00')
-const date2 = new Date(2018, 4, 22, 15, 13, 10, 22)
+const date1 = new Date("May 11 2018 09:00");
+const date2 = new Date(2018, 4, 22, 15, 13, 10, 22);
 
 now.setFullYear(1980);
 
 console.log(now);
-console.log(date1)
-console.log(date2)
+console.log(date1);
+console.log(date2);
 
 const convert = now.toDateString();
 console.log(convert);
