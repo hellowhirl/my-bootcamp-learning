a7e63910d6cabd4a188a55a348706d5e7e81f468
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-01-26 04:07:51 +0900

    ES6 notes on import and export

--- webpack-test/src/circle.js
+++ webpack-test/src/circle.js
@@ -1,11 +1,15 @@
+//
+// by default everything we define here will be private, unless we use the 'export' keyword
 const _radius = new WeakMap();
 
+// when we use the 'export' keyword before a class (or object) it will be exported to the outside
+// 'import' will be able to use this class but we won't have access to '_radius' WeakMap
 export class Circle {
   constructor(radius) {
     _radius.set(this, radius);
   }
 
   draw() {
-    console.log('Circle with radius ' + _radius.get(this));
+    console.log("Circle with radius " + _radius.get(this));
   }
 }
--- webpack-test/src/index.js
+++ webpack-test/src/index.js
@@ -1,4 +1,6 @@
 import { Circle } from "./circle";
+// we use 'import' to import other object from our modules
+// without using Webpack a temporary workaround would be to use './circle.js'
 
 const c = new Circle(10);
 c.draw();
