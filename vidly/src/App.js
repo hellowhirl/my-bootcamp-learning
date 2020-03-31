2ce2f2b948eeab2f67768b28bf97681f3a3e8350
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-03-31 13:55:16 +0900

    vidly - get current user with JSON web token

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import jwtDecode from "jwt-decode"; // this module exports a deafult function, which we can name as anything
+// Good practice to have some organization in our import statements - above modules are 3rd party libraries
 import NavBar from "./components/navBar";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
@@ -8,13 +10,29 @@ import MovieForm from "./components/movieForm";
 import NotFound from "./components/notFound";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+// Above are components from our application, below are our CSS modules
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token"); // return a JSON web token
+      const user = jwtDecode(jwt); // if we pass null, "empty string", or invalid JSON token then we'll get an exception
+      // console.log(user);
+      this.setState({ user });
+    } catch (ex) {
+      // we ignore, because this is not an application error
+      // this is only for the case when there is no Jwt in local storage (i.e an anonymous user)
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
             {/* for MovieForm we are using route parameter (match.params) */}
